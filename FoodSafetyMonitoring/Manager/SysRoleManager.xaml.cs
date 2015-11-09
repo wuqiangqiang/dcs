f746965eaeaa3d3c2f167a125e59c66e0c9021d0
zhujing <289400663@qq.com>
2015-11-09 17:37:15 +0800

    角色列表

--- FoodSafetyMonitoring/Manager/SysRoleManager.xaml.cs
+++ FoodSafetyMonitoring/Manager/SysRoleManager.xaml.cs
@@ -25,11 +25,35 @@ namespace FoodSafetyMonitoring.Manager
     public partial class SysRoleManager : UserControl
     {
         private DbHelperMySQL dbHelper = null;
-
+        private System.Data.DataTable dt_role_flag = new System.Data.DataTable();
         private DataTable currentTable = new DataTable();
         public SysRoleManager()
         {
             InitializeComponent();
+
+            //赋值角色级别下拉选择框
+            dt_role_flag.Columns.Add(new DataColumn("levelid"));
+            dt_role_flag.Columns.Add(new DataColumn("levelname"));
+            var row2 = dt_role_flag.NewRow();
+            row2["levelid"] = "1";
+            row2["levelname"] = "省级";
+            dt_role_flag.Rows.Add(row2);
+            var row3 = dt_role_flag.NewRow();
+            row3["levelid"] = "2";
+            row3["levelname"] = "市(州)";
+            dt_role_flag.Rows.Add(row3);
+            var row4 = dt_role_flag.NewRow();
+            row4["levelid"] = "3";
+            row4["levelname"] = "区县";
+            dt_role_flag.Rows.Add(row4);
+            var row5 = dt_role_flag.NewRow();
+            row5["levelid"] = "4";
+            row5["levelname"] = "检测单位";
+            dt_role_flag.Rows.Add(row5);
+            ComboboxTool.InitComboboxSource(role_flag, dt_role_flag, "lr");
+
+            //赋值检测环节下拉选择框
+            ComboboxTool.InitComboboxSource(role_type, "select typeId,typeName from t_dept_type", "lr");
 
             this.Loaded += new RoutedEventHandler(Test_Loaded);
 
@@ -38,7 +62,7 @@ namespace FoodSafetyMonitoring.Manager
         void Test_Loaded(object sender, RoutedEventArgs e)
         {
             BindData();
-            Clear();
+            //Clear();
         }
 
         /// <summary>
@@ -47,10 +71,9 @@ namespace FoodSafetyMonitoring.Manager
         /// 
         private void BindData()
         {
-
             DataTable dt = null;
             lvlist.DataContext = null;
-            string strSql = string.Format("select NUMB_ROLE,INFO_NAME,INFO_EXPL,FLAG_TIER FROM sys_client_role where cuserid = '{0}'", (Application.Current.Resources["User"] as UserInfo).ID);
+            string strSql = string.Format("select NUMB_ROLE,INFO_NAME,INFO_EXPL,FLAG_TIER,roletype FROM sys_client_role where cuserid = '{0}'", (Application.Current.Resources["User"] as UserInfo).ID);
             try
             {
                 dbHelper = DbHelperMySQL.CreateDbHelper();
@@ -64,31 +87,6 @@ namespace FoodSafetyMonitoring.Manager
             }
         }
 
-        /// <summary>
-        /// 从内存表中查询用户
-        /// </summary>
-        /// <param name="userName">用户名</param>
-        /// <param name="userType">角色类型</param>
-        private void SelectUser(string userName)
-        {
-            if (userName != "")
-            {
-                DataRow[] drs = currentTable.Select("INFO_NAME like '%" + userName + "%'");
-                DataTable temp = currentTable.Clone();
-                foreach (DataRow row in drs)
-                {
-                    DataRow dr = temp.NewRow();
-                    dr.ItemArray = row.ItemArray;
-                    temp.Rows.Add(dr);
-                }
-                lvlist.DataContext = temp;
-            }
-            else
-            {
-                lvlist.DataContext = currentTable;
-            }
-        }
-
         //判断角色下是否存在人员
         private bool isExistRoleName(string oid)
         {
@@ -96,19 +94,8 @@ namespace FoodSafetyMonitoring.Manager
             bool result = false;
             try
             {
-                if (this.txt_RoleName.Text.Trim() == "")
-                {
-                    DataRow[] drs = currentTable.Select("NUMB_ROLE=" + oid);
+                sql = string.Format("select RECO_PKID from sys_client_user where ROLE_ID ='{0}'", oid);
 
-                    string INFO_ROLE = drs[0]["INFO_CODE"].ToString();
-
-                    sql = string.Format("select FK_CODE_ROLE from sys_client_user where  FK_CODE_ROLE ='{0}'", INFO_ROLE);
-
-                }
-                else
-                {
-                    sql = string.Format("select FK_CODE_ROLE from sys_client_user where FK_CODE_ROLE ='{0}'", this.txt_RoleName.Tag.ToString());
-                }
                 if (Exists(sql))
                 {
                     result = true;
@@ -144,10 +131,15 @@ namespace FoodSafetyMonitoring.Manager
         }
         private void Clear()
         {
+            this.role_details.Visibility = Visibility.Visible;
             this.txt_RoleName.IsEnabled = false;
             this.txt_RoleExplain.IsEnabled = false;
+            this.role_flag.IsEnabled = false;
+            this.role_type.IsEnabled = false;
             this.txt_RoleName.Text = "";
             this.txt_RoleExplain.Text = "";
+            this.role_flag.SelectedIndex = 0;
+            this.role_type.SelectedIndex = 0;
             this.btnSave.Tag = null;
         }
         private void btnDelete_Click(object sender, RoutedEventArgs e)
@@ -170,7 +162,7 @@ namespace FoodSafetyMonitoring.Manager
                 }
                 else
                 {
-                    Toolkit.MessageBox.Show("角色下存在人员，不能删除！", "系统提示");
+                    Toolkit.MessageBox.Show("角色已分配给用户，不能删除！", "系统提示");
                     return;
                 }
             }
@@ -199,8 +191,11 @@ namespace FoodSafetyMonitoring.Manager
         }
         private void btnModify_Click(object sender, RoutedEventArgs e)
         {
+            Clear();
             this.txt_RoleName.IsEnabled = true;
             this.txt_RoleExplain.IsEnabled = true;
+            this.role_flag.IsEnabled = true;
+            this.role_type.IsEnabled = true;
 
             string id = (sender as Button).Tag.ToString();
             DataRow[] drs = currentTable.Select("NUMB_ROLE=" + id);
@@ -209,9 +204,26 @@ namespace FoodSafetyMonitoring.Manager
                 this.txt_RoleName.Tag = drs[0]["NUMB_ROLE"].ToString();
                 this.txt_RoleName.Text = drs[0]["INFO_NAME"].ToString();
                 this.txt_RoleExplain.Text = drs[0]["INFO_EXPL"].ToString();
+                for (int i = 0; i < role_flag.Items.Count; i++)
+                {
+                    if ((role_flag.Items[i] as Label).Tag.ToString() == drs[0]["FLAG_TIER"].ToString())
+                    {
+                        role_flag.SelectedItem = role_flag.Items[i];
+                        break;
+                    }
+                }
+                for (int i = 0; i < role_type.Items.Count; i++)
+                {
+                    if ((role_type.Items[i] as Label).Tag.ToString() == drs[0]["roletype"].ToString())
+                    {
+                        role_type.SelectedItem = role_type.Items[i];
+                        break;
+                    }
+                }
                 this.btnSave.Tag = id;
             }
         }
+
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
             if (this.txt_RoleName.Text.Trim() == "")
@@ -220,19 +232,23 @@ namespace FoodSafetyMonitoring.Manager
                 return;
             }
 
-
             //根据btnSave按钮的Tag属性判断是添加还是修改（null为添加，反之为修改）
             string strSql = string.Empty;
             if (btnSave.Tag == null)
             {
-                strSql = string.Format("call p_insert_role({0},'{1}','{2}','{3}')",
-                        (Application.Current.Resources["User"] as UserInfo).ID, txt_RoleName.Text,  txt_RoleExplain.Text);
+                strSql = string.Format("call p_insert_role({0},'{1}','{2}','{3}','{4}')",
+                        (Application.Current.Resources["User"] as UserInfo).ID, txt_RoleName.Text,txt_RoleExplain.Text,
+                         role_flag.SelectedIndex < 1 ? "" : (role_flag.SelectedItem as Label).Tag.ToString(),
+                         role_type.SelectedIndex < 1 ? "" : (role_type.SelectedItem as Label).Tag.ToString());
             }
             else
             {
-                strSql = string.Format(@"update sys_client_role set INFO_NAME='{0}',INFO_EXPL='{1}',MUSERID='{2}',INFO_NOTE='{3}'
-                                                         where NUMB_ROLE={4} ", txt_RoleName.Text, txt_RoleExplain.Text.Trim(), (Application.Current.Resources["User"] as UserInfo).ID,
-                                     btnSave.Tag.ToString());
+                strSql = string.Format(@"update sys_client_role set INFO_NAME='{0}',INFO_EXPL='{1}',FLAG_TIER='{2}',roletype='{3}',cuserid ='{4}'   
+                                         where NUMB_ROLE={5} ", txt_RoleName.Text, txt_RoleExplain.Text.Trim(),
+                                           role_flag.SelectedIndex < 1 ? "" : (role_flag.SelectedItem as Label).Tag.ToString(),
+                                           role_type.SelectedIndex < 1 ? "" : (role_type.SelectedItem as Label).Tag.ToString(), 
+                                          (Application.Current.Resources["User"] as UserInfo).ID,
+                                          btnSave.Tag.ToString());
             }
 
             try
@@ -263,12 +279,13 @@ namespace FoodSafetyMonitoring.Manager
             Clear();
         }
 
-
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
             Clear();
             this.txt_RoleName.IsEnabled = true;
             this.txt_RoleExplain.IsEnabled = true;
+            this.role_flag.IsEnabled = true;
+            this.role_type.IsEnabled = true;
         }
 
 
